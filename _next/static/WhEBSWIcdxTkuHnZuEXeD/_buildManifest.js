2fdc1848dbce1e78d90fbf0897ca3947858b52c8
Zachary Eggert <Eggert.Zachary@gmail.com>
2021-11-13 15:08:51 -0800

    update

---
+++ _next/static/WhEBSWIcdxTkuHnZuEXeD/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-51e7ad303d7a4bfa.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
